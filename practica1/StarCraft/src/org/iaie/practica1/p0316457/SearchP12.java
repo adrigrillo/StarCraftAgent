27316cb0d1f55c2ddc20d2f33a42366d38f7e324
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2016-10-27 23:41:48 +0200

    Realizado punto 2

--- practica1/StarCraft/src/org/iaie/practica1/p0316457/SearchP12.java
+++ practica1/StarCraft/src/org/iaie/practica1/p0316457/SearchP12.java
@@ -8,31 +8,35 @@ import org.iaie.search.Successor;
 import org.iaie.search.algorithm.Astar;
 
 import jnibwapi.JNIBWAPI;
+import jnibwapi.Position;
+import jnibwapi.Position.PosType;
 
 public class SearchP12 extends Astar{
-	private JNIBWAPI map;
+	private JNIBWAPI bwapi;
 	
 	public SearchP12(JNIBWAPI map) {
 		super(map.getMap());
-		this.map = map;
+		this.bwapi = map;
 
 	}
 
 	@Override
 	public List<Successor> generateSuccessors(Point actualState) {
 		List<Successor> listaSucesores = new ArrayList<Successor> ();
-		Point punto = new Point ();
-
-		for (int x = actualState.x-1; x<=x+1;x++){
-			for(int y = actualState.y-1;y<=y+1;y++){
-				if(x>=0 && y>=0 && x<map.getMap().getSize().getBX() && y<map.getMap().getSize().getBY()){
-					punto.setLocation(x, y);
-					Successor sucesor = new Successor(punto);
-					listaSucesores.add(sucesor);
+		
+		/* Tomamos todos los posibles sucesores de la posicion actual, comprobamos los
+		 * validos y los añadimos a la lista de sucesores */
+		for (int x = actualState.x - 1; x <= actualState.x + 1; x++){
+			for(int y = actualState.y - 1; y <= actualState.y + 1; y++){
+				// Comprobamos que no se mete como sucesor al mismo elemento
+				if (!new Point(x, y).equals(new Point(actualState.x, actualState.y))){
+					if(bwapi.getMap().isWalkable(new Position(x, y, PosType.WALK))){
+						listaSucesores.add(new Successor(new Point(x, y)));
+					}
 				}
 			}
 		}
-				
+		
 		return listaSucesores;
 	}
 
