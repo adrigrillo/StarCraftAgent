1626447814b571f83c407ec4573b054a33671581
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2016-11-08 16:17:07 +0100

    Ya ejecuta :D

--- tutorial3/Starcraft/src/org/iaie/tutorial3/PlayerTutorial0316457.java
+++ tutorial3/Starcraft/src/org/iaie/tutorial3/PlayerTutorial0316457.java
@@ -7,20 +7,36 @@ import org.iaie.btree.task.composite.Sequence;
 import org.iaie.btree.util.GameHandler;
 
 import jnibwapi.BWAPIEventListener;
+import jnibwapi.JNIBWAPI;
 import jnibwapi.Position;
 
 public class PlayerTutorial0316457 extends Agent implements BWAPIEventListener{
 	
 	// Creamos el arbol de decision
 	private BehavioralTree collectTree;
+	
+	public PlayerTutorial0316457() {            
 
-	@Override
+        // Generacion del objeto de tipo agente
+
+        // Creación de la superclase Agent de la que extiende el agente, en este método se cargan            
+        // ciertas variables de de control referentes a los parámetros que han sido introducidos 
+        // por teclado. 
+        super();
+        // Creación de una instancia del connector JNIBWAPI. Esta instancia sólo puede ser creada
+        // una vez ya que ha sido desarrollada mediante la utilización del patrón de diseño singlenton.
+        this.bwapi = new JNIBWAPI(this, true);
+        // Inicia la conexión en modo cliente con el servidor BWAPI que está conectado directamente al videojuego.
+        // Este proceso crea una conexión mediante el uso de socket TCP con el servidor. 
+        this.bwapi.start();
+    }
+
+	
 	public void connected() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("IAIE: Conectando con BWAPI");
 	}
 
-	@Override
+	
 	public void matchStart() {
 		BehaviourTree arbol = new BehaviourTree(bwapi);
 		Selector<GameHandler> collectResources = new Selector<GameHandler>("collectResources", new CollectMineral("CollectMineral", arbol), new CollectGas("CollectGas", arbol));
@@ -31,117 +47,80 @@ public class PlayerTutorial0316457 extends Agent implements BWAPIEventListener{
 		collectTree.addChild(train);
 	}
 
-	@Override
+	
 	public void matchFrame() {
 		collectTree.run();
 	}
-
-	@Override
+	
 	public void matchEnd(boolean winner) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
-	@Override
+	
 	public void keyPressed(int keyCode) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void sendText(String text) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
-	@Override
+	
 	public void receiveText(String text) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
-	@Override
+	
 	public void playerLeft(int playerID) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 
-	@Override
 	public void nukeDetect(Position p) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
 
-	@Override
 	public void nukeDetect() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
-
-	@Override
+	
 	public void unitDiscover(int unitID) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
-
-	@Override
+	
 	public void unitEvade(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
 
-	@Override
 	public void unitShow(int unitID) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
-	@Override
+	
 	public void unitHide(int unitID) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
-
-	@Override
+	
 	public void unitCreate(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void unitDestroy(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void unitMorph(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void unitRenegade(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void saveGame(String gameName) {
 		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
 	public void unitComplete(int unitID) {
 		// TODO Auto-generated method stub
-		
 	}
 
-	@Override
 	public void playerDropped(int playerID) {
 		// TODO Auto-generated method stub
-		
 	}
-
 }
